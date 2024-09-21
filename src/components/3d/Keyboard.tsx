b52119e6c4c8770451e35cf53ceff7f58103fdf8
Jesica <jestsee@gmail.com>
2024-09-21 21:43:31 +0700

    chore: setup tailwind, eslint & prettier

---
+++ src/components/3d/Keyboard.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline'
+
+export default function Keyboard() {
+  return (
+    <Spline scene='https://prod.spline.design/kSYZQ8RAOPSJH6W5/scene.splinecode' />
+  )
+}
