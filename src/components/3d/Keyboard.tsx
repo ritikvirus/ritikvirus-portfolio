13d4f3d2fef228d157b7fff64786dc4a8f461bc0
Jesica <jestsee@gmail.com>
2024-09-23 17:50:48 +0700

    feat: add social media icons

--- src/components/3d/Keyboard.tsx
+++ src/components/3d/Keyboard.tsx
@@ -2,7 +2,6 @@ import Spline from '@splinetool/react-spline'
 
 export default function Keyboard() {
   return (
-    // <Spline scene='https://prod.spline.design/HYIAoVcXNzElLRH8/scene.splinecode' />
     <Spline scene='https://prod.spline.design/A9nQBRKDCBQr2-b0/scene.splinecode' />
   )
 }
