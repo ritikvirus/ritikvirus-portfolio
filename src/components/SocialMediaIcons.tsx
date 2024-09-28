fb6fba7272b32e5ecf9820c8961e3fed19f81a51
Jesica <jestsee@gmail.com>
2024-09-28 13:19:54 +0700

    chore: refactor folder structure & add marquee component

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -4,7 +4,7 @@ import { LinkedIn } from './icons/LinkedIn'
 
 const SocialMediaIcons = () => {
   return (
-    <div className='flex gap-6'>
+    <div className='flex gap-8'>
       <Github className='size-8' />
       <Gitlab className='size-8' />
       <LinkedIn className='size-8' />
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,42 +10,68 @@ module.exports = {
   ],
   prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px'
-      }
-    },
-    extend: {
-      fontFamily: {
-        sans: ['DM Sans Variable', 'sans-serif'],
-        serif: ['Instrument Serif', 'serif']
-      },
-      screens: {
-        xs: '480px'
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' }
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
-        },
-        gradient: {
-          to: {
-            backgroundPosition: 'var(--bg-size) 0'
-          }
-        }
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        gradient: 'gradient 8s linear infinite'
-      }
-    }
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	extend: {
+  		fontFamily: {
+  			sans: ['DM Sans Variable', 'sans-serif'],
+  			serif: ['Instrument Serif', 'serif']
+  		},
+  		screens: {
+  			xs: '480px'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			gradient: {
+  				to: {
+  					backgroundPosition: 'var(--bg-size) 0'
+  				}
+  			},
+  			marquee: {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			gradient: 'gradient 8s linear infinite',
+  			marquee: 'marquee var(--duration) infinite linear',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
+  		}
+  	}
   },
   plugins: [require('tailwindcss-animate')]
 }
