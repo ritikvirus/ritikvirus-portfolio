7806af35c82ef9b9fe6e997b09f5caecd7c41d9e
Jesica <jestsee@gmail.com>
2024-09-28 18:06:38 +0700

    feat: add separator to bottom navigation bar

--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -3,6 +3,10 @@ import { HandWaving } from './icons/HandWaving'
 import { Briefcase } from './icons/Briefcase'
 import { ChatTeardropDots } from './icons/ChatTeardrop'
 import { Bookmarks } from './icons/Bookmarks'
+import { Github } from './icons/Github'
+import { LinkedIn } from './icons/LinkedIn'
+import { Envelope } from './icons/Envelope'
+import { Separator } from './ui/separator'
 
 const bottomNavigationItems = [
   {
@@ -27,11 +31,35 @@ const bottomNavigationItems = [
   }
 ]
 
+const socialMediaItems = [
+  {
+    name: 'Email',
+    icon: Envelope,
+    href: ''
+  },
+  {
+    name: 'LinkedIn',
+    icon: LinkedIn,
+    href: ''
+  },
+  {
+    name: 'Github',
+    icon: Github,
+    href: ''
+  }
+]
+
 const BottomNavigationBar = () => {
   return (
     <div className='fixed bottom-8 z-50'>
       <Dock direction='middle'>
         {bottomNavigationItems.map(({ name, icon: Icon, href }) => (
+          <DockIcon key={name}>
+            <Icon className='size-6' />
+          </DockIcon>
+        ))}
+        <Separator orientation='vertical' className='h-full' />
+        {socialMediaItems.map(({ name, icon: Icon, href }) => (
           <DockIcon key={name}>
             <Icon className='size-6' />
           </DockIcon>
