ac533b1940b4e4c095c71cf3db173bb5a659e992
Jesica <jestsee@gmail.com>
2024-10-24 19:41:13 +0700

    chore: update base url for hono client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,6 +2,6 @@ import { hc } from 'hono/client'
 
 import type { APIType } from '@/pages/api/[...path]'
 
-const client = hc<APIType>('http://localhost:4321')
+const client = hc<APIType>(import.meta.env.BASE_URL)
 
 export default client
