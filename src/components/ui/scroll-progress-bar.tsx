6e38c1c0f4e1866659315410daba8c0a3292c6ed
Jesica <jestsee@gmail.com>
2024-11-07 21:11:07 +0700

    chore: update scroll progress bar

--- src/components/ui/scroll-progress-bar.tsx
+++ src/components/ui/scroll-progress-bar.tsx
@@ -21,16 +21,22 @@ export default function ScrollProgressBar({
   position = 'bottom-right',
   color = 'hsl(var(--primary))',
   strokeSize = 2,
-  showPercentage = false
+  showPercentage = true
 }: Readonly<ScrollProgressBarType>) {
   const { scrollYProgress } = useScroll()
+  const barRef = React.useRef<HTMLSpanElement>(null)
+  const circleRef = React.useRef<HTMLSpanElement>(null)
 
   const scrollPercentage = useTransform(scrollYProgress, [0, 1], [0, 100])
 
-  const [percentage, setPercentage] = React.useState(0)
-
   useMotionValueEvent(scrollPercentage, 'change', (latest) => {
-    setPercentage(Math.round(latest))
+    if (barRef.current) {
+      barRef.current.style.width = `${latest}%`
+    }
+
+    if (circleRef.current) {
+      circleRef.current.textContent = `${Math.round(latest)}%`
+    }
   })
 
   if (type === 'bar') {
@@ -40,10 +46,10 @@ export default function ScrollProgressBar({
         style={{ height: `${strokeSize + 2}px` }}
       >
         <span
-          className='bg-priamry block h-full w-full'
+          ref={barRef}
+          className='bg-primary block h-full w-full'
           style={{
-            backgroundColor: color,
-            width: `${percentage}%`
+            backgroundColor: color
           }}
         ></span>
       </div>
@@ -52,40 +58,34 @@ export default function ScrollProgressBar({
 
   return (
     <div
-      className={cn('fixed flex items-center justify-center', {
+      className={cn('fixed z-30 flex items-center justify-center', {
         'end-0 top-0': position === 'top-right',
         'bottom-0 end-0': position === 'bottom-right',
         'start-0 top-0': position === 'top-left',
         'bottom-0 start-0': position === 'bottom-left'
       })}
     >
-      {percentage > 0 && (
-        <>
-          <svg width='100' height='100' viewBox='0 0 100 100'>
-            <circle
-              cx='50'
-              cy='50'
-              r='30'
-              fill='none'
-              strokeWidth={strokeSize}
-            />
-            <motion.circle
-              cx='50'
-              cy='50'
-              r='30'
-              pathLength='1'
-              stroke={color}
-              fill='none'
-              strokeDashoffset='0'
-              strokeWidth={strokeSize}
-              style={{ pathLength: scrollYProgress }}
-            />
-          </svg>
-          {showPercentage && (
-            <span className='absolute ml-2 text-sm'>{percentage}%</span>
-          )}
-        </>
+      {/* {percentage > 0 && (
+        <> */}
+      <svg width='100' height='100' viewBox='0 0 100 100'>
+        <circle cx='50' cy='50' r='30' fill='none' strokeWidth={strokeSize} />
+        <motion.circle
+          cx='50'
+          cy='50'
+          r='30'
+          pathLength='1'
+          stroke={color}
+          fill='none'
+          strokeDashoffset='0'
+          strokeWidth={strokeSize}
+          style={{ pathLength: scrollYProgress }}
+        />
+      </svg>
+      {showPercentage && (
+        <span ref={circleRef} className='absolute mx-auto text-sm'></span>
       )}
+      {/* </>
+      )} */}
     </div>
   )
 }
