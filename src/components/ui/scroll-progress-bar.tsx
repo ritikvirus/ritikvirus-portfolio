7c5ad68f3879d4c55deca646cf080823a20799fb
Jesica <jestsee@gmail.com>
2024-11-11 09:28:40 +0700

    chore: refactor collection's config

--- src/components/ui/scroll-progress-bar.tsx
+++ src/components/ui/scroll-progress-bar.tsx
@@ -32,7 +32,8 @@ export default function ScrollProgressBar({
     if (!spanRef.current) return
 
     if (type === 'bar') {
-      return (spanRef.current.style.width = `${latest}%`)
+      spanRef.current.style.width = `${latest}%`
+      return
     }
 
     spanRef.current.textContent = `${Math.round(latest)}%`
@@ -50,7 +51,7 @@ export default function ScrollProgressBar({
       >
         <span
           style={{ backgroundColor: color }}
-          className='absolute block h-full w-screen bg-gradient-to-r from-emerald-400 to-lime-200'
+          className='absolute block h-full w-screen bg-gradient-to-r from-emerald-400 to-emerald-200'
         ></span>
       </span>
     )
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,41 @@
-import { blogCollection } from './_schemas/blog'
-import { projectCollection } from './_schemas/project'
+import { defineCollection, z } from 'astro:content'
+
+import { iconSchema } from './_icons'
+
+export const blog = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional()
+  })
+})
+
+export const projects = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      year: z.number(),
+      title: z.string(),
+      numberOfTeamMembers: z.number(),
+      description: z.string(),
+      heroImage: image(),
+      links: z.array(
+        z.object({
+          icon: iconSchema,
+          name: z.string(),
+          url: z.string()
+        })
+      ),
+      updatedDate: z.coerce.date().optional()
+    })
+})
 
 export const collections = {
-  blog: blogCollection,
-  projects: projectCollection
+  blog,
+  projects
 }
