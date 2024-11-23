210c1aabac4ba4309c2066e36153cf13a62dec45
Jesica <jestsee@gmail.com>
2024-11-23 18:47:50 +0700

    chore: remove unnecessary logic from scroll progress bar

--- src/components/ui/scroll-progress-bar.tsx
+++ src/components/ui/scroll-progress-bar.tsx
@@ -1,20 +1,11 @@
-import {
-  motion,
-  useMotionValueEvent,
-  useScroll,
-  useSpring,
-  useTransform
-} from 'framer-motion'
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion'
 import React from 'react'
 
 import { cn } from '@/lib/utils'
 
 interface ScrollProgressBarType {
-  type?: 'circle' | 'bar'
-  position?: 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left'
   color?: string
   strokeSize?: number
-  showPercentage?: boolean
   targetId?: string
 }
 
@@ -25,11 +16,8 @@ const getContainerElement = (id?: string) => {
 }
 
 export default function ScrollProgressBar({
-  type = 'circle',
-  position = 'bottom-right',
   color = 'azure',
   strokeSize = 2,
-  showPercentage = true,
   targetId
 }: Readonly<ScrollProgressBarType>) {
   const spanRef = React.useRef<HTMLSpanElement>(null)
@@ -50,67 +38,21 @@ export default function ScrollProgressBar({
     restDelta: 0.001
   })
 
-  const scrollPercentage = useTransform(widthValue, [0, 1], [0, 100])
-
-  useMotionValueEvent(scrollPercentage, 'change', (latest) => {
-    if (!spanRef.current) return
-
-    if (type === 'bar') {
-      spanRef.current.style.width = `${latest}%`
-      return
-    }
-
-    spanRef.current.textContent = `${Math.round(latest)}%`
-  })
-
-  if (type === 'bar') {
-    return (
-      <span
-        ref={spanRef}
-        className={cn(
-          'pointer-events-none fixed end-0 start-0 top-0 z-30',
-          'w-0 overflow-clip rounded-full'
-        )}
-        style={{ height: `${strokeSize + 2}px` }}
-      >
-        <span
-          style={{ backgroundColor: color }}
-          className='bg-primary-gradient absolute block h-full w-screen'
-        ></span>
-      </span>
-    )
-  }
+  const width = useTransform(widthValue, [0, 1], ['0%', '100%'])
 
   return (
-    <div
-      className={cn('fixed z-30 flex items-center justify-center', {
-        'end-0 top-0': position === 'top-right',
-        'bottom-0 end-0': position === 'bottom-right',
-        'start-0 top-0': position === 'top-left',
-        'bottom-0 start-0': position === 'bottom-left'
-      })}
-    >
-      {/* {percentage > 0 && (
-        <> */}
-      <svg width='100' height='100' viewBox='0 0 100 100'>
-        <circle cx='50' cy='50' r='30' fill='none' strokeWidth={strokeSize} />
-        <motion.circle
-          cx='50'
-          cy='50'
-          r='30'
-          pathLength='1'
-          stroke={color}
-          fill='none'
-          strokeDashoffset='0'
-          strokeWidth={strokeSize}
-          style={{ pathLength: scrollYProgress }}
-        />
-      </svg>
-      {showPercentage && (
-        <span ref={spanRef} className='absolute mx-auto text-sm'></span>
+    <motion.span
+      ref={spanRef}
+      className={cn(
+        'pointer-events-none fixed end-0 start-0 top-0 z-30',
+        'w-0 overflow-clip rounded-full'
       )}
-      {/* </>
-      )} */}
-    </div>
+      style={{ height: `${strokeSize + 2}px`, width }}
+    >
+      <span
+        style={{ backgroundColor: color }}
+        className='bg-primary-gradient absolute block h-full w-screen'
+      ></span>
+    </motion.span>
   )
 }
