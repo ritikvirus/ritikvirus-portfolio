53999a1a51d8415fe78a429a62de77100ba5321a
Jesica <jestsee@gmail.com>
2024-11-30 09:57:20 +0700

    feat: add hover effect on github contributions chart

--- src/components/TableOfContent/TableOfContent.tsx
+++ src/components/TableOfContent/TableOfContent.tsx
@@ -64,7 +64,7 @@ const TableOfContent = ({ headings, className }: Props) => {
       <ul
         className={cn(
           'mt-2 space-y-1.5 text-[0.9rem] text-zinc-600',
-          'scrollbar-color max-h-[640px] overflow-scroll'
+          'scrollbar-color max-h-[480px] overflow-scroll'
         )}
       >
         {groupedHeadings.map((heading) => {
--- src/pages/_components/bento/BentoItemGithubActivity/BentoItemGithubActivityChart.tsx
+++ src/pages/_components/bento/BentoItemGithubActivity/BentoItemGithubActivityChart.tsx
@@ -1,5 +1,6 @@
 import { Github } from '@icons/Github'
 import HeatMap, { type SVGProps } from '@uiw/react-heat-map'
+import React from 'react'
 
 import { formatDate, getDateSuffix } from '@/lib/utils'
 import type { GithubContributionData } from '@/types'
@@ -14,15 +15,34 @@ const getDateProps = () => {
   return { startDate: sixMonthsAgo, endDate: today }
 }
 
+const renderRect =
+  (handleMouseEnter: (date: string) => void): SVGProps['rectRender'] =>
+  (props, data) => {
+    const date = new Date(data.date)
+    const formattedDate =
+      date.toLocaleDateString('en-US', { day: 'numeric', month: 'long' }) +
+      getDateSuffix(date.getDate())
+    const tileInfo = `${data.count || 'No'} contributions on ${formattedDate}`
+
+    return <rect onMouseEnter={() => handleMouseEnter(tileInfo)} {...props} />
+  }
+
 interface Props extends GithubContributionData {}
 
 const BentoGithubActivity = (props: Props) => {
+  const [hoveredTile, setHoveredTile] = React.useState<string | null>(null)
+
   return (
-    <div className='relative flex h-full flex-col justify-between px-4 pb-5 pt-4 max-md:gap-4'>
+    <div
+      onMouseLeave={() => setHoveredTile(null)}
+      className='relative flex h-full flex-col justify-between px-4 pb-5 pt-4 max-md:gap-4'
+    >
       <div className='flex items-baseline justify-between gap-4 max-xs:flex-col'>
         <BentoBadge icon={Github} text='Github activity' />
         <p className='line-clamp-1 text-sm'>
-          {props.totalContributions ?? 'No'} contributions in the last year
+          {hoveredTile
+            ? hoveredTile
+            : `${props.totalContributions ?? 'No'} contributions in the last year`}
         </p>
       </div>
       <div className='w-full overflow-x-scroll'>
@@ -37,7 +57,7 @@ const BentoGithubActivity = (props: Props) => {
           style={{ color: '#fff' }}
           rectProps={{ rx: 4 }}
           rectSize={16}
-          // rectRender={renderRect}
+          rectRender={renderRect((date) => setHoveredTile(date))}
           panelColors={{
             1: '#19222F',
             4: '#0F4E43',
