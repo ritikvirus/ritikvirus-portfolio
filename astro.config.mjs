45e3af3b7cee999f6884402f07f57036e5093969
Jesica <jestsee@gmail.com>
2024-12-02 20:28:23 +0700

    chore: setup partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import playformCompress from '@playform/compress'
 import { defineConfig } from 'astro/config'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
-
-import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,20 +26,27 @@ export default defineConfig({
     }
   },
 
-  integrations: [mdx({
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'wrap',
-          headingProperties: {
-            class: 'article-heading'
+  integrations: [
+    mdx({
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: 'wrap',
+            headingProperties: {
+              class: 'article-heading'
+            }
           }
-        }
+        ]
       ]
-    ]
-  }), sitemap(), react(), tailwind({
-    applyBaseStyles: false
-  }), playformCompress()]
+    }),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    playformCompress(),
+    partytown()
+  ]
 })
