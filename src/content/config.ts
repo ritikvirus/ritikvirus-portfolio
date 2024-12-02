19f1ac61da3632a952c3c5353bacca7e40ffb7dd
Jesica <jestsee@gmail.com>
2024-12-02 17:33:00 +0700

    chore: update projects config

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,27 +18,33 @@ export const blog = defineCollection({
 export const projects = defineCollection({
   type: 'content',
   schema: ({ image }) =>
-    z.object({
-      year: z.number(),
-      title: z.string(),
-      htmlTitle: z.string(),
-      description: z.string(),
-      heroImage: image(),
-      readingTime: z.number().optional(),
-      wordsCount: z.number().optional(),
-      contributors: z.array(
-        z.object({ name: z.string(), username: z.string(), url: z.string() })
-      ),
-      links: z.array(
-        z.object({
-          icon: iconSchema,
-          name: z.string(),
-          url: z.string()
-        })
-      ),
-      updatedDate: z.coerce.date().optional(),
-      latestCommitUrl: z.string().optional()
-    })
+    z
+      .object({
+        year: z.number(),
+        title: z.string(),
+        htmlTitle: z.string().optional(),
+        description: z.string(),
+        heroImage: image(),
+        heroImageAlign: z.enum(['top', 'center']).default('top'),
+        links: z
+          .array(
+            z.object({
+              icon: iconSchema,
+              name: z.string(),
+              url: z.string()
+            })
+          )
+          .optional()
+          .default([]),
+        readingTime: z.number().optional(),
+        wordsCount: z.number().optional(),
+        updatedDate: z.coerce.date().optional(),
+        latestCommitUrl: z.string().optional()
+      })
+      .transform((data) => ({
+        ...data,
+        htmlTitle: data.htmlTitle || data.title
+      }))
 })
 
 export const collections = {
