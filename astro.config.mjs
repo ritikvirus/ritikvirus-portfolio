df4f3354af47e9e8f245f014406a27952e424ecf
Jesica <jestsee@gmail.com>
2024-12-09 13:31:29 +0700

    chore: setup local build preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
+import node from '@astrojs/node'
 import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
@@ -10,9 +11,15 @@ import { defineConfig } from 'astro/config'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 
+let adapter = vercel()
+
+if (process.argv[3] === '--node' || process.argv[4] === '--node') {
+  adapter = node({ mode: 'standalone' })
+}
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter,
   output: 'hybrid',
   site: 'https://jestsee.com',
 
